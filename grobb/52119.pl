94df6386ab808608566fcd6d6389ea1f3bfab82a
FVFippy <44533609+FVFippy@users.noreply.github.com>
2020-01-21 07:09:41 -0500

    Update 52119.pl
    Updated the say dialog to be consistent with Alla.

--- grobb/52119.pl
+++ grobb/52119.pl
@@ -6,7 +6,7 @@ sub EVENT_SPAWN {
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::emote("stops playing a lively tune.");
-		quest::say("Come to enjoy my music?");
+		quest::say("Here I stand high and above, a minstrel supreme, my words offer love. Love between all, troll and ogre alike. I sing to all, except Tabaz, so take a hike!'");
 	}
 	elsif ($text=~/come back to the closet/i) {
 		quest::say("No!! I'm getting so good!! Even these trolls appreciate my music! <sigh>...fine I'll go. But before I do, can you do me a favor first? I'd like to study a new instrument. I hear that some lizardmen near the ogre city make an interesting instrument called a fife. If you can get me a few samples, say four, just to make sure I get a good quality one, I'll gladly go back in the closet.");
@@ -26,7 +26,7 @@ sub EVENT_TIMER {
 sub EVENT_ITEM {
 	#:: Match four 12198 - Lizardman Scout Fife
 	if (plugin::takeItems(12198 => 4)) {
-		quest::say("Oh good, you got four!! I hope at least one of them is of decent quality. Well, guess I should keep up my end of the bargain. Let's go.");
+		quest::say("All right! I was kinda hoping the lizards would finish you off and I could stay free, but a deal is a deal. Let's go.");
 		#:: Give a 12216 - The Minstrel
 		quest::summonitem(12216);
 		#:: Ding!
